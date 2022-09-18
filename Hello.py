ea2447c61868ae35dde4f8a9db7879ab6a6f89ad
yayaythegreat <yayaythegreat@gmail.com>
2022-09-18 15:21:02 +0800

    Python testings

---
+++ Hello.py
@@ -0,0 +1,26 @@
+
+loc=input("Input Location:")
+date=input("Input Date:")
+time1=input("[1] Input Time:")
+temp1=input("[1] Input Temperature in Celcius:")
+time2=input("[2] Input Time:")
+temp2=input("[2] Input Temperature in Celcius:")
+time3=input("[3] Input Time:")
+temp3=input("[3] Input Temperature in Celcius:")
+print("You have succesfull input a log!")
+print("=======Log Entry=======")
+print("Location:",loc)
+print("Date:",date)
+print("Time take:",time1,time2,time3)
+print("Average Temperature:")
+C1=tem1+temp2+temp3
+Celcius=C1/3
+Farenheit=Celcius*1.8+32
+kelvin=Celcius+273.15
+Rakine=Celcius*1.8+32+459.67
+print("Celsius:",Celcius)
+print("Farenheit:",Farenheit)
+print("kelvin:",kelvin)
+print("Rankine:",Rakine)
+print("=======End of log======")
+
